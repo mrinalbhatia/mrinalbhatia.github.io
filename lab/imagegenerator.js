c684f357fb94907ba93d73715aa19ff4d9cd4d8d
mrinalbhatia <bhatm188@newschool.edu>
2015-11-20 00:19:31 +0000

    msg ...

--- lab/imagegenerator.js
+++ lab/imagegenerator.js
@@ -40,3 +40,35 @@ $(document).ready(function() {
       $(this).css({background: '#ffffff'});
    });
 });
+
+$(window).mouseover(function() {
+    $(".box-set12").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("top",topPosition).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+
+$(window).mouseover(function() {
+    $(".box-set15").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("top",topPosition).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+
+$(window).mouseover(function() {
+    $(".box-set9").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("top",topPosition).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
---
+++ studio/typeweb1.js
@@ -0,0 +1,187 @@
+
+
+$(window).mouseover(function() {
+    $(".box-set20").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set8").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set5").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set25").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set1").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set18").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set2").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set19").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set6").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set7").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set3").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set15").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+
+$(window).mouseover(function() {
+    $(".box-set9").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+$(window).mouseover(function() {
+    $(".box-set12").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+
+$(window).mouseover(function() {
+    $(".box-set22").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+
+$(window).mouseover(function() {
+    $(".box-set10").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+
+$(window).mousemove(function() {
+    $(".box-set4").each(function() {
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  $(this).css("border-color",newColor);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
